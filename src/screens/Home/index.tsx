0462ef2dfb3bbdb64bc8419e468c63d1b7a6f2b5
giovani <giovaniocan@gmail.com>
2023-07-28 14:34:02 -0300

    Header is finished

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import { styles } from "./styles";
+import { Header } from "../../components/Header";
+
+export function Home(){
+    return(
+        <View style={styles.container}>
+             <Header />
+        </View>
+    )
+}
